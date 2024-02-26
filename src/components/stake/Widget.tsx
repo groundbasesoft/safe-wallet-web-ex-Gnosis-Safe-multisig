3a55cc18bd1e6c27471a495ac3cfa6c2fc490ab3
Daniel Dimitrov <daniel@compojoom.com>
2024-02-26 15:13:00 +0100

    update widget to v 0.0.107

--- src/components/stake/Widget.tsx
+++ src/components/stake/Widget.tsx
@@ -1,12 +1,23 @@
-"use client"
-
-import "@stakekit/widget/package/css";
-import { SKApp, darkTheme } from "@stakekit/widget"
+import '@stakekit/widget/package/css'
+import { SKApp, darkTheme, lightTheme } from '@stakekit/widget'
+import useSafeWalletProvider from '../../services/safe-wallet-provider/useSafeWalletProvider'
+import { useDarkMode } from '../../hooks/useDarkMode'
+import css from './styles.module.css'
 
 export const Widget = () => {
+  const wallet = useSafeWalletProvider()
+  const darkMode = useDarkMode()
+
+  if (!wallet) return null
+
+  console.log('wallet', wallet)
   return (
-    <>
-      <SKApp apiKey="api-key-here" theme={darkTheme} connectKitForceTheme="darkMode" />
-    </>
+    <main className={css.widgetRoot}>
+      <SKApp
+        theme={darkMode ? darkTheme : lightTheme}
+        externalProviders={{ type: 'safe_wallet', provider: wallet }}
+        apiKey="3e82ff42-9fc4-49a7-b9b4-66da4d7c0f04"
+      />
+    </main>
   )
 }
